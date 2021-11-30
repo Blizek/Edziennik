0be2ebde59520d1f990ae3773424d26daeb31d5d
Blizek <blazej.naziemiec@gmail.com>
2021-11-30 16:29:45 +0100

    new screen

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -19,6 +20,7 @@ public class Main extends Application {
 
         Scene scene = new Scene(anchorPane);
         stage.setScene(scene);
+        stage.getIcons().add(new Image("/images/logo.png"));
         stage.setTitle("Electronic School Diary");
         stage.setResizable(false);
 
