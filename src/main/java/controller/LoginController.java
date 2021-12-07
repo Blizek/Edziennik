5ca6e7226d6f3fafcdc1d0d77fba703dc770acc9
Blizek <blazej.naziemiec@gmail.com>
2021-12-07 16:06:59 +0100

    logging algorithm optimisation (not tested)

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,19 +1,15 @@
 package controller;
 
-import DAO.DAOUser;
 import com.jfoenix.controls.JFXToggleButton;
-import features.SetRememberMeData;
+import features.LoggingAlgorithm;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
-import model.User;
 import routings.GoToForgotPasswordScreen;
 import routings.GoToHomeScreen;
-import variables.UserIDHolder;
 
 import java.sql.SQLException;
-import java.util.List;
 
 public class LoginController {
     @FXML
@@ -42,30 +38,9 @@ public class LoginController {
     public void checkData() throws SQLException {
         String emailAddress = userEmail.getText();
         String password = userPassword.getText();
-        DAOUser userDAO = new DAOUser();
-        List<User> possibleUsers = userDAO.getByEmail(emailAddress);
-        if (possibleUsers.isEmpty()) wrongDataText.setVisible(true);
-        else {
-            boolean userFound = false;
-            User lookingUser = null;
-            for (User user: possibleUsers) {
-                if (user.getUser_email().equals(emailAddress) && user.getUser_password().equals(password)) {
-                    lookingUser = user;
-                    userFound = true;
-                    break;
-                }
-            }
-            if (!userFound) wrongDataText.setVisible(true);
-            else {
-                if (rememberMeToggleButton.isSelected()) {
-                    new SetRememberMeData().setData(userEmail.getText(), userPassword.getText());
-                }
-                wrongDataText.setVisible(false);
-                System.out.println("Logged as: ID: " + lookingUser.getUser_id() + ", e-mail: " + lookingUser.getUser_email()
-                + ", password: " + lookingUser.getUser_password() + ", role: " + lookingUser.getUser_role());
-                UserIDHolder.setUserID(lookingUser.getUser_id());
-                new GoToHomeScreen().runThis(mainController);
-            }
-        }
+        boolean result = LoggingAlgorithm.algorithm(emailAddress, password, rememberMeToggleButton);
+
+        if (!result) wrongDataText.setVisible(true);
+        else new GoToHomeScreen().runThis(mainController);
     }
 }
---
+++ src/main/java/features/LoggingAlgorithm.java
@@ -0,0 +1,28 @@
+package features;
+
+import DAO.DAOUser;
+import com.jfoenix.controls.JFXToggleButton;
+import model.User;
+import variables.UserIDHolder;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class LoggingAlgorithm {
+    public static boolean algorithm(String email, String password, JFXToggleButton rememberMeToggleButton) throws SQLException {
+        DAOUser userDao = new DAOUser();
+        List<User> users = userDao.getByEmail(email);
+        if (users.isEmpty()) return false;
+        else {
+            User user = users.get(0);
+            if (!user.getUser_email().equals(email) || !user.getUser_password().equals(password)) return false;
+            else {
+                if (rememberMeToggleButton.isSelected()) new SetRememberMeData().setData(email, password);
+                System.out.println("Logged as: ID: " + user.getUser_id() + ", e-mail: " + user.getUser_email()
+                        + ", password: " + user.getUser_password() + ", role: " + user.getUser_role());
+                UserIDHolder.setUserID(user.getUser_id());
+                return true;
+            }
+        }
+    }
+}
